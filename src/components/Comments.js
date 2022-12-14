17ff2f1fdde6897e4c4938d061c4ee617b29ed55
Magda <114444914+magdaV24@users.noreply.github.com>
2022-12-14 14:25:20 +0200

    Update Comments.js

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,30 @@
-import './CommentForm.css'
-import React from 'react'
-import Comment from './Comment'
+import "./CommentForm.css";
+import React, { useEffect, useState } from "react";
+import { projectDatabase } from "../firebase/config";
+import Comment from "./Comment";
 
-export default function Comments({document}) {
+export default function Comments() {
+  const [commentList, setCommentList] = useState();
 
-    
-    return (
-        <div>
-            {document.comments && document.comments.map((comment) =>(
-                <Comment comment = {comment} key={comment.id}/>
-            ))}
-        </div>
-    )
+  useEffect(() => {
+    const dataRef = projectDatabase.ref("comments");
+    dataRef.on("value", (snapshot) => {
+      const comments = snapshot.val();
+      const commentList = [];
+      for (let i in comments) {
+        commentList.push({ i, ...comments[i] });
+      }
+      console.log(commentList);
+      setCommentList(commentList);
+    });
+  }, []);
+
+  return (
+    <section>
+      {commentList &&
+        commentList.map((comment) => (
+          <Comment comment={comment} key={comment.id} />
+        ))}
+    </section>
+  );
 }

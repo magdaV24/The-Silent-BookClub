af9a69bef915fb47c5085c3486b36db889ed8c18
Magda <114444914+magdaV24@users.noreply.github.com>
2022-12-14 14:20:59 +0200

    Update Comment.js
    Added a function to edit the parent comment.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,96 +1,113 @@
-import './CommentForm.css'
-import React, { useState } from 'react'
-import Avatar from './Avatar'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReply, faSquarePen, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
-import CommentForm from './CommentForm'
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
-import { useFirestore } from '../hooks/useFirestore'
-import { useCollection } from '../hooks/useCollection'
+import "./CommentForm.css";
+import React, { useState } from "react";
+import Avatar from "./Avatar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReply,
+  faSquarePen,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
+import CommentForm from "./CommentForm";
+import { projectDatabase } from "../firebase/config";
 
-const EditBox = ( {comment} ) => {
+const EditBox = ({ comment, commentUid }) => {
+  const dbRef = projectDatabase.ref("comments");
+  const [edit, setEdit] = useState(comment.content);
 
+  const editComment = (e) => {
+    e.preventDefault();
 
-    return (
-        <form className='edit-box'>
-            <textarea>
-                {comment.content}
-        </textarea>
-        <button>Edit Comment!</button>
-        </form>
-    )
-}
+    const editMade = dbRef.child(`${commentUid}`);
+    editMade.update({
+      content: edit,
+    });
 
-const Comment = ({ comment }) => {
+    setEdit("");
+  };
 
-    const { updateDocument } = useFirestore('event-list')
-    const { documents } = useCollection('event-list')
+  return (
+    <form className="edit-box" onSubmit={editComment}>
+      <textarea onChange={(e) => setEdit(e.target.value)} value={edit}>
+        {edit}
+      </textarea>
+      <button>Edit Comment!</button>
+    </form>
+  );
+};
 
+export default function Comment({ comment }) {
+  const [isReplaying, setIsReplaying] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const [isReplaying, setIsReplaying] = useState(false)
-    const [isEditing, setIsEditing] = useState(false)
-
-    const giveLike = async (e) =>{
-        e.preventDefault()
-
-        let like = comment.likes
-
-         await updateDocument(comment.id, {
-            likes: like + 1
-         })
-
-         console.log(comment.likes)
-
-    }
-
-
-    return (
-        <div className='box'>
-            <div key={comment.id}>
-                <li className='comment'>
-                    <div className='user'>
-                        <Avatar src={comment.photoURL} />
-                        <p>{comment.displayName}</p>
-                    </div>
-                    <div className='comment-date'>
-                        <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</p>
-                    </div>
-                    <div className='content'>
-                        <p>{comment.content}</p>
-                    </div>
-                    <div className='buttons'>
-                        <div className='actions'>
-                            <button className="replay" onClick={() => setIsReplaying(prev => !prev)}><FontAwesomeIcon icon={faReply} /></button>
-                            <button className="edit" onClick={() => setIsEditing(prev => !prev)}><FontAwesomeIcon icon={faSquarePen} /></button>
-                        </div>
-                        <div className='reactions'>
-                            <div className='like'>
-                                <button className='like_btn' onClick={giveLike}><FontAwesomeIcon icon={faThumbsUp} /></button>
-                                <div className='likes'>{comment.likes}</div>
-                            </div>
-                            <div className='like'>
-                                <button className='dislike_btn'><FontAwesomeIcon icon={faThumbsDown} /></button>
-                                <div className='dislikes'>{comment.dislikes}</div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <div className='reply-box'>
-                    {isReplaying && (
-                        <CommentForm parentId={comment.id} project={documents} parentComment={comment} />
-                    )}
-                </div>
-
-                <div className='editing-box'>
-                    {isEditing && (
-                        <EditBox comment={comment} project={comment}/>
-                    )}
-                </div>
-
+  return (
+    <div className="box">
+      <div key={comment.id}>
+        <li className="comment">
+          <div className="user">
+            <Avatar src={comment.photoURL} />
+            <p>{comment.displayName}</p>
+            <p className="comment-date">{comment.createdAt}</p>
+          </div>
+          <div className="content">
+            <p>{comment.content}</p>
+          </div>
+          <div className="buttons">
+            <div className="actions">
+              <button
+                className="replay"
+                onClick={() => setIsReplaying((prev) => !prev)}
+              >
+                <FontAwesomeIcon icon={faReply} />
+              </button>
+              <button
+                className="edit"
+                onClick={() => setIsEditing((prev) => !prev)}
+              >
+                <FontAwesomeIcon icon={faSquarePen} />
+              </button>
             </div>
+            <div className="reactions">
+              <form className="like">
+                <button className="like_btn">
+                  <FontAwesomeIcon icon={faThumbsUp} />
+                </button>
+                <div className="likes">{comment.likes}</div>
+              </form>
+              <form className="like">
+                <button className="dislike_btn">
+                  <FontAwesomeIcon icon={faThumbsDown} />
+                </button>
+                <div className="dislikes">{comment.dislikes}</div>
+              </form>
+            </div>
+          </div>
+        </li>
+        <div className="reply-box">
+          {isReplaying && (
+            <CommentForm
+              parentId={comment.id}
+              parentUid={comment.i}
+              parentComment={comment}
+            />
+          )}
         </div>
-    )
+
+        <div className="editing-box">
+          {isEditing && <EditBox comment={comment} commentUid={comment.i} />}
+        </div>
+
+        <div className="children-box">
+          {comment.children &&
+            comment.children.map((child) => {
+              return (
+                <div className="child-comment" key={Math.random()}>
+                  <Comment comment={child} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-export default Comment

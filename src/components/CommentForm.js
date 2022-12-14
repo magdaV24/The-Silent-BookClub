c2576d3ae6748c57a0d96f3f4862204733c41fdb
Magda <114444914+magdaV24@users.noreply.github.com>
2022-12-14 14:23:47 +0200

    Update CommentForm.js
    The comments are now saved in Firebase's Realtime Database.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,63 +1,74 @@
-/* eslint-disable no-loop-func */
-import React from 'react'
-import { useState } from 'react'
-import { useAuthContext } from '../hooks/useAuthContext'
-import './CommentForm.css'
-import { useFirestore } from '../hooks/useFirestore'
-import { timestamp } from '../firebase/config'
+import React from "react";
+import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import "./CommentForm.css";
+import { timestamp, projectDatabase } from "../firebase/config";
+import uuid from "react-uuid";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
-export default function CommentForm({ project, parentId, parentComment }) {
+export default function CommentForm({ parentId, parentUid, parentComment }) {
+  const [comm, setComm] = useState("");
+  const { user } = useAuthContext();
 
-    const [comm, setComm] = useState('')
-    const { user } = useAuthContext()
-    const { updateDocument,  response } = useFirestore('event-list')
-  
+  const submitComment = async (e) => {
+    e.preventDefault();
 
-    const submitComment = async (e) => {
-        e.preventDefault()
+    try {
+      let newComment = {
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        content: comm,
+        createdAt: formatDistanceToNow(
+          timestamp.fromDate(new Date()).toDate(),
+          { addSuffix: true }
+        ),
+        parentId: null || parentId,
+        id: uuid(),
+        likes: 0,
+        dislikes: 0,
+        children: [],
+      };
 
-        let newComment = {
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-            content: comm,
-            createdAt: timestamp.fromDate(new Date()),
-            parentId: parentId || null,
-            id: Math.random(),
-            children: [],
-            likes: 0,
-            dislikes: 0
-        }
+      const dbRef = projectDatabase.ref("comments");
+      let commentList = [];
 
-        if (parentId == null) {
-            await updateDocument(project.id, {
-                comments: [ ...project.comments, newComment ]
-            })
+      if (!parentId) {
+        const newSetComment = dbRef.push();
+        newSetComment.set(newComment);
+        console.log(parentId);
+      }
+
+      if (parentId) {
+        if (parentComment.children) {
+          parentComment.children.push(newComment);
+          const commentChild = dbRef.child(`${parentUid}`);
+          commentChild.update({
+            children: parentComment.children,
+          });
         } else {
-            await updateDocument(project.id, {
-                children: [ ...parentComment.children, newComment ]
-            })
+          commentList.push(newComment);
+          const commentChild = dbRef.child(`${parentUid}`);
+          commentChild.update({
+            children: commentList,
+          });
         }
-
-       
-
-        if (!response.error) {
-            setComm('')
-        }
-
-
+      }
+      setComm("");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <form className='comment-form' onSubmit={submitComment}>
-            <label>
-                <textarea
-                    required
-                    onChange={(e) => setComm(e.target.value)}
-                    value={comm}
-                >
-                </textarea>
-            </label>
-            <button>Add Comment!</button>
-        </form>
-    )
+  return (
+    <form className="comment-form" onSubmit={submitComment}>
+      <label>
+        <textarea
+          required
+          onChange={(e) => setComm(e.target.value)}
+          value={comm}
+        ></textarea>
+      </label>
+      <button>Add Comment!</button>
+    </form>
+  );
 }
